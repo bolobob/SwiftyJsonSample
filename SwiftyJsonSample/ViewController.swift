b2abcccd3816222eed4e3a34e6b2bdef2259d1ac
bolobob <aokishinichi@gmail.com>
2015-05-31 21:38:38 +0900

    Initial Commit

---
+++ SwiftyJsonSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyJsonSample
+//
+//  Created by 青木 真一 on 2015/05/31.
+//  Copyright (c) 2015年 bolobob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
